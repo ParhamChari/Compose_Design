70ca001907a0656dddfc22ed518f13d7c2431a6b
Parham <parhamchary@gmail.com>
2023-08-30 00:26:14 +0430

    Initial commit

--- app/src/main/java/com/example/composedesign/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/composedesign/ui/view/MainActivity.kt
@@ -3,11 +3,8 @@ package com.example.composedesign.ui.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composedesign.R
-import com.example.composedesign.data.model.MessageModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
